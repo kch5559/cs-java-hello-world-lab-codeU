d5be73b0f91760d3b08abb892117ec341b312f04
Chohee <kch5559@gmail.com>
2016-03-18 19:06:07 -0700

    First Lab

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,19 @@
 package com.flatironschool.javacs;
+import java.lang.*;
+
 
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+	String strVersion = System.getProperty("java.version");
+	System.out.println(strVersion);
+	String version = strVersion.substring(0,3);
+	System.out.println(version);
+	return Double.parseDouble(version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    // you can test the output of getVersion()
+	HelloWorld.getVersion();
     }
 }
